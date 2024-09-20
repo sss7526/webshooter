5bac87b0a162a243b9124ea21dd4e96617c748b8
SSS <142176901+sss7526@users.noreply.github.com>
2024-09-20 14:06:21 -0400

    Update validator.go

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -2,10 +2,18 @@ package validator
 
 import (
 	"regexp"
+	"strings"
 )
 
 func IsValidURL(target string) bool {
 	var urlPattern = regexp.MustCompile(`^(https?:\/\/)?([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}(\/[\w\-\.~:\/?#[\]@!$&'()*+,;=%]*)?$`)
 
 	return urlPattern.MatchString(target)
+}
+
+func EnsureScheme(target string) string {
+	if !strings.HasPrefix(target, "http://") && !strings.HasPrefix(target, "https://") {
+		target = "https://" + target
+	}
+	return target
 }
