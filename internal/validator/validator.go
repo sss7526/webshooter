9e8fa48c49c923b619ccf0ee2ecc84ea16314a06
SSS <142176901+sss7526@users.noreply.github.com>
2024-09-20 14:51:01 -0400

    Update validator.go

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -12,8 +12,10 @@ func IsValidURL(target string) bool {
 }
 
 func EnsureScheme(target string) string {
-	if !strings.HasPrefix(target, "http://") && !strings.HasPrefix(target, "https://") {
+	if !strings.HasPrefix(target, "http://") && !strings.HasPrefix(target, "https://") && !strings.HasPrefix(target, "localhost") {
 		target = "https://" + target
+	} else if strings.HasPrefix(target, "localhost") {
+		target = "http://" + target
 	}
 	return target
 }
