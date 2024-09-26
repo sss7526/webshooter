a7c66d73d2a47f1c98590e102492a2cd8b94597b
SSS <142176901+sss7526@users.noreply.github.com>
2024-09-26 13:11:30 -0400

    targetOptions struct to hold bools for processor function parameters

---
+++ internal/target_options.go
@@ -0,0 +1,49 @@
+package processor
+
+type targetOptions struct {
+	verbose 	bool
+	saveToImage	bool
+	saveToPDF	bool
+	translate	bool
+	useTorProxy	bool
+}
+
+type option func(*targetOptions)
+
+func WithVerbose(verbose bool) option {
+	return func(opts *targetOptions) {
+		opts.verbose = verbose
+	}
+}
+
+func WithSaveToImage(save bool) option {
+	return func(opts *targetOptions) {
+		opts.saveToImage = save
+	}
+}
+
+func WithSaveToPDF(save bool) option {
+	return func(opts *targetOptions) {
+		opts.saveToPDF = save
+	}
+}
+
+func WithTranslate(translate bool) option {
+	return func(opts *targetOptions) {
+		opts.translate = translate
+	}
+}
+
+func WithTorProxy(useTor bool) option {
+	return func(opts *targetOptions) {
+		opts.translate = translate
+	}
+}
+
+func NewTargetOptions(options ...option) *targetOptions {
+	opts := &targetOptions{}
+	for _, opt := range options {
+		opt(opts)
+	}
+	return opts
+}
